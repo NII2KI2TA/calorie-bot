645170c778681288d34418434360877c14d4a34a
Melkonyan Airenik <114315967+airmely@users.noreply.github.com>
2026-05-26 01:58:07 +0300

    add flask

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+import os
 import asyncio
+import threading
+
+from flask import Flask
 
 from aiogram import (
     Bot,
@@ -12,25 +16,50 @@ from aiogram.fsm.storage.memory import (
 from handlers.user import router
 
 
-import os
-
 TOKEN = os.getenv("BOT_TOKEN")
-
 
 bot = Bot(token=TOKEN)
 
 storage = MemoryStorage()
 
-dp = Dispatcher(
-    storage=storage
-)
+dp = Dispatcher(storage=storage)
 
 dp.include_router(router)
 
 
-async def main():
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+
+    return "Bot is running!"
+
+
+def run_web():
+
+    port = int(
+        os.environ.get("PORT", 10000)
+    )
+
+    app.run(
+        host="0.0.0.0",
+        port=port
+    )
+
+
+async def run_bot():
 
     await dp.start_polling(bot)
+
+
+async def main():
+
+    threading.Thread(
+        target=run_web
+    ).start()
+
+    await run_bot()
 
 
 if __name__ == "__main__":
