9718a5cc167b4cca8e1701b7b9b9bc3b916b15fa
Melkonyan Airenik <114315967+airmely@users.noreply.github.com>
2026-05-29 02:26:39 +0300

    fix render bot2

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import os
 import asyncio
+from threading import Thread
+
+from flask import Flask
 
 from aiogram import Bot, Dispatcher
 from aiogram.fsm.storage.memory import MemoryStorage
@@ -8,39 +11,44 @@ from aiogram.client.session.aiohttp import AiohttpSession
 from handlers.user import router
 
 
-# TOKEN
-
 TOKEN = os.getenv("BOT_TOKEN")
 
-
-# SESSION
-
 session = AiohttpSession()
-
-
-# BOT
 
 bot = Bot(
     token=TOKEN,
     session=session
 )
 
-
-# STORAGE
-
 storage = MemoryStorage()
-
-
-# DISPATCHER
 
 dp = Dispatcher(storage=storage)
 
 dp.include_router(router)
 
 
-# START BOT
+# FLASK
 
-async def main():
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return "Bot is running!"
+
+
+def run_web():
+    port = int(os.environ.get("PORT", 10000))
+
+    app.run(
+        host="0.0.0.0",
+        port=port
+    )
+
+
+# BOT
+
+async def run_bot():
 
     print("BOT STARTED")
 
@@ -55,4 +63,10 @@ async def main():
 
 if __name__ == "__main__":
 
-    asyncio.run(main())
+    web_thread = Thread(
+        target=run_web
+    )
+
+    web_thread.start()
+
+    asyncio.run(run_bot())
